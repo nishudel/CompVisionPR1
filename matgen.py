469aa7ddcf4d9f6cda640c1dd20280f8bf5de44f
nishudel <nish@udel.edu>
2020-10-11 21:25:45 -0400

    Modified some files

--- matgen.py
+++ matgen.py
@@ -26,3 +26,215 @@ def xshear(shx):
 def yshear(shy):
 	yshmat=np.float32([[1,0,0],[shy,1,0],[0,0,1]])	
 	return yshmat	
+
+#Affine Transformation-Non-Homogenous
+def block_haff(x,y):
+	block=[[x,y,1,0,0,0],[0,0,0,x,y,1]]
+	return block
+
+def get_haff_nh(pointsar):
+	B=np.zeros((6,1))
+	i=0
+	j=1
+	while(i<6):
+		B[i]=B[i]+pointsar[j][0]
+		i=i+1
+		B[i]=B[i]+pointsar[j][1]
+		j=j+2
+		i=i+1
+
+	i=0	
+	while(i<5):
+		blk=np.array(block_haff(pointsar[i][0],pointsar[i][1]))
+		if (i==0):
+			A=np.array(blk)
+		else:
+			A=np.concatenate((A,blk),0)		
+		i=i+2
+
+	H_aff=np.linalg.inv(A)@B
+	H_aff=np.reshape(H_aff,(2,3))
+
+	return H_aff	
+
+#Perspective Transformation-Non-Homogenous
+def block_hper(x,y,x_t,y_t):
+	block=[[x,y,1,0,0,0,-x_t*x,-x_t*y],[0,0,0,x,y,1,-y_t*x,-y_t*y]]
+	return block
+
+def get_hper_nh(pointsar):
+	B=np.zeros((8,1))
+	i=0
+	j=1
+	while(i<8):
+		B[i]=B[i]+pointsar[j][0]
+		i=i+1
+		B[i]=B[i]+pointsar[j][1]
+		j=j+2
+		i=i+1
+	i=0	
+	while(i<7):
+		blk=np.array(block_hper(pointsar[i][0],pointsar[i][1],pointsar[i+1][0],pointsar[i+1][1]))
+		if (i==0):
+			A=np.array(blk)
+		else:
+			A=np.concatenate((A,blk),0)		
+		i=i+2	
+	
+	H_1=np.linalg.inv(A)@B
+	H_per=np.ones((9,1))
+	k=0
+	while(k<8):
+		H_per[k]=H_1[k]
+		k=k+1
+	H_per=np.reshape(H_per,(3,3))	
+	return H_per
+
+
+
+#Affine Transformation Matrix : Homogenous method
+def block_af_h(x,y,x_t,y_t):
+	block=[[-x,-y,-1,0,0,0,x_t],[0,0,0,-x,-y,-1,y_t]]
+	return block
+
+def get_haff_h(n,pointsar):
+	i=0
+	while (i<n):
+		blk=np.array(block_af_h(pointsar[i][0],pointsar[i][1],pointsar[i+1][0],pointsar[i+1][1]))
+		
+		if (i==0):
+			A=np.array(blk)
+		else:
+			A=np.concatenate((A,blk),0)
+		i=i+2
+
+	U, s, vh = np.linalg.svd(A,True)	
+	end=6
+	vh1=vh.T/vh.T[end][end]
+	H1= np.zeros(9)
+	H1[8]=1
+	i=0
+	while (i<6):
+		H1[i]=vh1[i][6]
+		i=i+1
+	#print(H1)
+	H1=np.reshape(H1,(3,3))
+
+	return H1
+
+#Perspective Transformation Matrix : Homogenous method
+def block_per_h(x,y,x_t,y_t):
+	block=[[-x,-y,-1,0,0,0,x_t*x,x_t*y,x_t],[0,0,0,-x,-y,-1,y_t*x,y_t*y,y_t]]
+	return block
+
+def get_hper_h(n,pointsar):
+	i=0
+	while (i<n):
+		blk=np.array(block_per_h(pointsar[i][0],pointsar[i][1],pointsar[i+1][0],pointsar[i+1][1]))
+		
+		if (i==0):
+			A=np.array(blk)
+		else:
+			A=np.concatenate((A,blk),0)
+		i=i+2
+
+	U, s, vh = np.linalg.svd(A,True)	
+	end=8
+	vh1=vh.T/vh.T[end][end]
+	H1= np.zeros(9)
+	H1[8]=1
+	i=0
+	while (i<8):
+		H1[i]=vh1[i][8]
+		i=i+1
+	H1=np.reshape(H1,(3,3))
+
+	return H1
+
+		
+
+
+
+
+
+
+
+
+
+
+
+def blockfn_inv(x,y,x_t,y_t):
+	block=[[x,y,1,0,0,0,-x_t*x,-x_t*y],[0,0,0,x,y,1,-y_t*x,-y_t*y]]
+	return block
+
+def generateA_inv(n,pointsar):
+	i=0
+	while (i<n):
+		i=i+2
+		blk=np.array(blockfn_inv(pointsar[i-2][0],pointsar[i-2][1],pointsar[i-1][0],pointsar[i-1][1]))
+		
+		if ((i-2)==0):
+			A_inv=np.array(blk)
+		else:
+			A_inv=np.concatenate((A_inv,blk),0)
+			
+	return A_inv
+
+def blockfn_pr(x,y,x_t,y_t):
+	block=[[-x,-y,-1,0,0,0,x_t*x,x_t*y,x_t],[0,0,0,-x,-y,-1,y_t*x,y_t*y,y_t]]
+	return block
+
+
+def blockfn_af(x,y,x_t,y_t):
+	block=[[-x,-y,-1,0,0,0],[0,0,0,-x,-y,-1]]
+	return block
+
+def generateA(n,pointsar):
+	i=0
+	if(n==8):
+		while (i<n):
+			i=i+2
+			blk=np.array(blockfn_pr(pointsar[i-2][0],pointsar[i-2][1],pointsar[i-1][0],pointsar[i-1][1]))
+			
+			if ((i-2)==0):
+				A=np.array(blk)
+			else:
+				A=np.concatenate((A,blk),0)
+			#print(A.shape)		
+	if(n==6):
+		while (i<n):
+			i=i+2
+			blk=np.array(blockfn_af(pointsar[i-2][0],pointsar[i-2][1],pointsar[i-1][0],pointsar[i-1][1]))
+			
+			if ((i-2)==0):
+				A=np.array(blk)
+			else:
+				A=np.concatenate((A,blk),0)
+			
+	return A
+
+def generateH(n,pointsar):	
+	A=generateA(n,pointsar)
+	#print(A.shape)
+	U, s, vh = np.linalg.svd(A,True)
+	end=vh.shape[1]-1
+	#print(end)
+	vh=vh/vh[end,end]
+	H2= np.zeros(9)
+	i=0
+	if(n==8):
+		while (i<9):
+			H2[i]=H2[i]+vh.T[i,8]
+			i=i+1
+		H2=np.reshape(H2,(3,3))
+		H=H2
+	else:
+		while (i<6):
+			H2[i]=H2[i]+vh.T[i,5]
+			i=i+1
+		H2=np.reshape(H2,(3,3))
+		H2[2,2]=1
+		H=H2
+
+	return H
+			
--- pr1b.py
+++ pr1b.py
@@ -1,16 +1,11 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import math as m
 import random
 import matgen 
 from matgen import *
-
-
-def blockfn(x,y,x_t,y_t):
-	block=[[-x,-y,-1,0,0,0,x_t*x,x_t*y,x_t],[0,0,0,-x,-y,-1,y_t*x,y_t*y,y_t]]
-	return block
-
 
 
 if __name__ == '__main__':
@@ -18,15 +13,10 @@ if __name__ == '__main__':
 	img1=cv2.imread('totoro.jpg')
 	img = img1[...,::-1].copy()
 	rows,cols,ch = img.shape
-	#cv2.imshow('image',img)
-	#k= cv2.waitKey(0) 
-
-
-
 	#setting random values 
 	theta=random.random()
-	tx=random.random()
-	ty=random.random()	
+	tx=500*random.random()
+	ty=500*random.random()	
 	sx=1.5
 	sy=0.9
 	#shifting centre and bringing back
@@ -34,27 +24,215 @@ if __name__ == '__main__':
 	py=0.5*cols
 	tM=matgen.translmat(-px,-py)
 	tiM=matgen.translmat(px,py)
+
+################################ PART I ################################
+	# choose the point in the original image and choose corresponding point the transformation
+	# go on for n such points
+	print("PART I:\n")
+	print("Press esc to go to part B after new images show up")
+	plt.subplot(121),plt.imshow(img),plt.title('Original')
+	plt.suptitle('Choose four points and their intended locations alternatively')
+	n=8
+	pointsls=plt.ginput(n,0)
+	plt.close( )
+
+	#Saving the points in np array
+	pointsar=np.array(pointsls)
 	
-	#affine transformation	
-	tfmat_af=matgen.translmat(tx,ty)@matgen.scalemat(sx,sy)@matgen.rotatmat(theta)
-	tfmat_af=tiM@tfmat_af@tM
+	#generating the Affine transformation matrix - using A_inverse method 
+	H_a=get_haff_h(6,pointsar)
+	H_af=np.zeros((2,3))
+	i=0
+	while(i<2):
+		j=0
+		while (j<3):
+			H_af[i,j]=H_a[i,j]
+			j=j+1
+		i=i+1	 
+	print("\nThe first two rows of the Affine Transformation Matrix: ")
+	print(H_af)
+
+	#generating the Perspective transformation matrix - using A_inverse method 	
+	H_per=get_hper_h(8,pointsar)
+	print("\nThe Complete Perspective Transformation Matrix: ")
+	print(H_per)
+	
+	img_pr=cv2.warpPerspective(img,H_per,(rows,cols))
+	img_af=cv2.warpAffine(img,H_af,(rows,cols))
+
+	
+	fig=plt.figure( )
+	gs=gridspec.GridSpec(2,2)
+	ax1=fig.add_subplot(gs[0,:]),plt.imshow(img),plt.title('Original')
+	ax2=fig.add_subplot(gs[1,0]),plt.imshow(img_pr),plt.title('Perspective')
+	ax3=fig.add_subplot(gs[1,1]),plt.imshow(img_af),plt.title('Affine')	
+	plt.draw()
+	plt.waitforbuttonpress(0) # this will wait for indefinite time
+	plt.close( )
+
+############################# PART II ###############################
+	print("\nPART II:\n")
+	print("A)Using exact number of points:Non Homogenous method ")
+	plt.subplot(121),plt.imshow(img),plt.title('Original')	
+	plt.subplot(122),plt.imshow(img_af),plt.title('Affine')
+	plt.suptitle('Choose three points and their corresponding locations alternatively on both images:')
+	n=6
+	points_aff=plt.ginput(n,0)
+	plt.close( )
+
+	plt.subplot(121),plt.imshow(img),plt.title('Original')	
+	plt.subplot(122),plt.imshow(img_pr),plt.title('Perspective ')
+	plt.suptitle('Choose four points and their corresponding locations alternatively on both images:')
+	n=8
+	points_per=plt.ginput(n,0)
+	plt.close( )
+	#generating the Perspective transformation matrix - using A_inverse method 	
+	H_affine=get_haff_nh(points_aff)
+	i=0
+	j=0
+	H_aff_nh=np.identity(3)
+	while (i<2):
+		j=0
+		while (j<3):
+			H_aff_nh[i][j]=H_affine[i][j]
+			j=j+1
+		i=i+1
+	print("\nThe Affine Transformation Matrix:Non-Homogenous method")
+	print(H_aff_nh)
+
+	#generating the Perspective transformation matrix - using A_inverse method 	
+	H_pers_nh=get_hper_nh(points_per)
+	print("\nThe Perspective Transformation Matrix:Non-Homogenous method ")
+	print(H_per_nh)
+
+	print("\n \nB)Using excess number of points:Homogenous method ")
+
+	plt.subplot(121),plt.imshow(img),plt.title('Original')	
+	plt.subplot(122),plt.imshow(img_af),plt.title('Affine')
+	plt.suptitle('Choose five points and their corresponding locations alternatively on both images:')
+	n1=10
+	points_aff=plt.ginput(n1,0)
+	plt.close( )
+	#generating the Affine transformation matrix - using Homogenous method
+	H_aff_exs=get_haff_h(n1,points_aff)
+	print("\nThe Affine Transformation Matrix:Homogenous method ")
+	print(H_aff_exs)
+	
+
+	plt.subplot(121),plt.imshow(img),plt.title('Original')	
+	plt.subplot(122),plt.imshow(img_pr),plt.title('Perspective ')
+	plt.suptitle('Choose six points and their corresponding locations alternatively on both images:')
+	n2=12
+	points_per=plt.ginput(n2,0)
+	plt.close( )
+	#generating the Perspective transformation matrix - Homogenous method 	
+	H_per_exs=get_hper_h(n2,points_per)
+	print("\nThe Perspective Transformation Matrix:Homogenous method ")
+	print(H_per_exs)
+
+
+	print("\n \nC)Using Using inbuilt functions and Comparision:")
+
+	'''
+	A_inv=generateA_inv(8,pointsar)
+	#print(A_inv.shape)
+
+	i=0
+	j=0
+
+	#B=pointsar[1][0]
+	while(i<4):
+		j=0
+		while(j<2):
+			elem=ptsfin[i][j]
+			if(i==0):
+				B=[ptsfin[0][0],ptsfin[0][1]]
+			else :
+				B=np.append(B,elem)
+			j=j+1	
+		i=i+1	
+	B=np.array(B)
+	
+
+	H1=A_inv@B
+	H1=np.append(H1,[1])
+	print(H1)
+	H=H1.reshape((3,3))
+	print(H)
+	'''
+	'''
+	A_pr=generateA(8,pointsar)
+	U, s, vh = np.linalg.svd(A_pr,True)
+	#print(vh)
+	H2_pr= np.zeros(9)
+	i=0
+	while (i<9):
+		H2_pr[i]=H2_pr[i]+vh.T[i,8]
+		i=i+1
+	#print(A@H)
+	H2_pr=np.reshape(H2_pr,(3,3))
+	H33=H2_pr[2,2]
+	H=H2_pr/H33
+	print('H',H)
+	'''
+	
+	
+	'''
+	plt.draw()
+	plt.waitforbuttonpress(0) # this will wait for indefinite time
+	plt.close( )
+
+	plt.subplot(121),plt.imshow(img),plt.title('Original')
+	plt.draw()
+	plt.waitforbuttonpress(0) # this will wait for indefinite time
+	plt.close( )
+	'''
+
+
+	'''
+
+
+	H1=generateH(8,pointsar)
+	#print(H1)
+	H2=generateH(6,pointsar)
+	#print(H2)
+	'''
+
+
+
+
+
+
+	
+	#affine transformation matrix : tfmat_afs	
+	tfmat1=matgen.translmat(tx,ty)@matgen.scalemat(sx,sy)@matgen.rotatmat(theta)
+	tfmat_af=tiM@tfmat1@tM
 	#print(tfmat_af)
+	
+	#perspective transformation matrix : tfmat_pr
+	tfmat_pr=tfmat1
+	tfmat_pr[2,0]=0.0008
+	tfmat_pr[2,1]=0.0004
+	tfmat_pr=tiM@tfmat_pr@tM
+	tfmat_pr=tfmat_pr/tfmat_pr[2,2]
+	#print('tfmat_pr:   ')
+	#print(tfmat_pr)
+	
 	#applying the transformation to the image	
 	mat_af=cv2.warpPerspective(img,tfmat_af,(rows,cols))
 	#cv2.imshow('img',mat_af)
 	#k= cv2.waitKey(0) 
 	
-	#perspective transformation
-	tfmat_pr=matgen.translmat(300,150)@matgen.scalemat(1,1)@matgen.rotatmat(m.pi/3)
-	tfmat_pr[2,0]=0.0005
-	tfmat_pr[2,1]=0.0005
-	tfmat_pr=tiM@tfmat_pr@tM
-	#print('tfmat_pr:   ')
-	print(tfmat_pr)
 	#applying the transformation to the image	
 	mat_pr=cv2.warpPerspective(img,tfmat_pr,(rows,cols))
 	#cv2.imshow('img',mat_pr)
 	#k= cv2.waitKey(0) 
+
+
+
+
+
+
 
 	#######The following is to test the validity of the algo - using points directly #######
 	X=np.array([[200,800,800,200],[100,100,700,700],[1,1,1,1]])
@@ -85,7 +263,9 @@ if __name__ == '__main__':
 	H=np.reshape(H,(3,3))
 	H33=H[2,2]
 	H=H/H33
-	print('H',H)
+	#print('H',H)
+
+	#print(tfmat_pr-H)
 	
 
 
@@ -153,6 +333,6 @@ if __name__ == '__main__':
 	pts1=np.float32(pt1)
 	pts2=np.float32(pt2)
 	M = cv2.getPerspectiveTransform(pts1,pts2)
-	print('Matric calculated by in built function')
+	print('Matrix calculated by in built function')
 	print(M)	
 	'''
